fc0a0296102d197aace54033ae94b2604fb12eff
Weslley Isidorio <weslleyisidorio@gmail.com>
2024-03-21 21:51:55 -0300

    Atualização media por ano

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import numpy as np
+import time
 
 
 def main():
@@ -17,9 +18,9 @@ def abrir_csv(nome_arq:str):
 
 def menu(dados):
     print('''------- MENU --------
-1 - Listar por cidade: 
-2 - Média por ano: 
-3 - Maior consumidor:
+1 - Listar media de preço por cidade: 
+2 - Média de preço por ano: 
+3 - :
 0 - Sair 
           ''')
     try:
@@ -30,7 +31,21 @@ def menu(dados):
 
     if escolha == 1:
         cidade = input("Cidade: ").strip().capitalize()
-        listar_cidade(cidade, dados)
+        linha  = listar_cidade(cidade, dados)
+        media = calcular_media_total(linha)
+        print(f'Media: {media:.1f}')
+    elif escolha == 2:
+        cidade = input("Cidade: ").strip().capitalize()
+        linha  = listar_cidade(cidade, dados)
+        try:
+            ano = int(input("Digite um ano: "))
+        except:
+            print("Entrada inválida!")
+            main()
+
+        mediaAnos = media_anos(ano, linha)
+        print(f'Media de consumo de maças de {ano} em {cidade}: {mediaAnos:.1f}')
+
     elif escolha == 0:
         exit()
 
@@ -40,11 +55,10 @@ def listar_cidade(nome:str, dados):
     for linha in dados:
         if linha[0] == nome:
             lin = linha
-            media = calcular_media(lin)
     return lin
         
 
-def calcular_media(dados):
+def calcular_media_total(dados):
     somatorio = 0
     for i in range(1, len(dados)-3):
         somatorio += float(dados[i])
@@ -53,7 +67,17 @@ def calcular_media(dados):
 
     return media
 
-def media(ano, dados):
+def calcular_media_ano(min, max, linha):
+    somatorio = 0
+    for i in range(min, max+1):
+        somatorio += float(linha[i])
+    print(somatorio)   
+    media = somatorio / 12
+    print(media)
+
+    return media
+
+def media_anos(ano, dados):
     anos = {2013: [1,12], 
             2014: [13,24],
             2015: [25,36],
@@ -61,11 +85,13 @@ def media(ano, dados):
             2017: [49,60],
             2018: [61,72],
             2019: [73,84]}
-    
+    min, max = anos[ano]
+    print(min, max)    
 
+    media = calcular_media_ano(min,max, dados)
+
+    return media
     
     
-
-
 if __name__ == "__main__":
     main()
